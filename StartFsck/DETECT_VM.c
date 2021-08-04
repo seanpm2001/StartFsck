a39de4475b17a5be5048375b8ecbccb160c9104b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 16:02:01 -0700

    DETECT_VM.c
    A script for detecting if the program is contained inside a virtual machine.

---
+++ StartFsck/DETECT_VM.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// "Pro-VM code" - Makes sure the program is running in a isolated virtual machine before starting
+// I am not very good at programming in C. This may appear as bad code and may not compile correctly.
+// Definitions
+union detectVM (1);
+// Main
+int main(void) {
+    return status()
+    if (status == "1"):
+      return startFsck_CanGo();
+    else:
+      return startFsck_Stop();
+    break;
+    exit;
+} // End of main method
+int status(); {
+  // Makes sure the program is running in a virtual machine before starting
+  check();
+  if ViMachine == true); {
+    status == 1;
+    break;
+  } else {
+    status == 0;
+    break;
+  }
+  break;
+}
+int startFsck_CanGo(): {
+if (status == 1); {
+  startFsck_CanGo() == false;
+  startFsck_Stop() == false;
+}}
+int startFsck_Stop(): {
+if (status == 0); {
+  startFsck_Stop() == true;
+  startFsck_CanGo() == false;
+}}
+int check() {
+ // Incomplete: the check function for checking if the virtual machine is present 
+}
+int Vimachine() {
+ // Incomplete: the virtual machine function for checking if the virtual machine is present 
+ ViMachine = bool(false) // Starter value
+}
+/* File info
+* File version: `1 (Wednesday, August 4th 2021 at 16:02 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 54
+*/
+// This script is incomplete, and needs lots of work
+// End of script
