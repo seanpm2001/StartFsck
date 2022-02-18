816b75aca6d1f3432e186936f6100a7ce292eab9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-17 18:57:47 -0800

    .STARTUP_V1.bat
    An archive of the first version of the startup script for this payload.

---
+++ StartFsck/ClockBrick/OldVersions/1/1-100/.STARTUP_V1.bat
@@ -0,0 +1,9 @@
+REM Start of script
+REM Startup script for ClockBrick, the startup clock bricking tool
+start "" "C:\Program Files\StartFsck\ClockBrick\ClockBrick.htm" /command:%1 %*/
+GOTO 3
+REM File info
+REM File type: Batchfile (*.bat)
+REM File version: 1 (2022, Thursday, February 17th at 6:57 pm)
+REM Line count (including blank lines and compiler line): 10
+REM End of script
